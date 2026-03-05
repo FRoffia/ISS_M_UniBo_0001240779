b932c2897830db7486c803b42aa369c60c2ec4c3
FRoffia <francescoroffia@gmail.com>
2026-03-05 10:25:37 +0100

    Work on LifeController

--- ConwayLife/Sprint1/conway26Java/src/main/java/conway/domain/LifeController.java
+++ ConwayLife/Sprint1/conway26Java/src/main/java/conway/domain/LifeController.java
@@ -1,5 +1,33 @@
 package main.java.conway.domain;
 
-public class LifeController {
+public class LifeController implements GameController{
+
+	public LifeController() {
+		
+	}
+	
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nextGeneration() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void switchCellStatus(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void auto(int n) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
