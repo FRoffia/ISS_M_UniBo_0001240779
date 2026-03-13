2d136f794330848dfdc47aa209ca9ab21bb14095
FRoffia <francescoroffia@gmail.com>
2026-03-13 10:23:57 +0100

    sprint reports

--- ConwayLife/Sprint1/conway26Java/src/main/java/conway/domain/GameController.java
+++ ConwayLife/Sprint1/conway26Java/src/main/java/conway/domain/GameController.java
@@ -1,8 +1,10 @@
 package main.java.conway.domain;
 
 public interface GameController {
-	void reset();//reset della griglia a stato iniziale
-	void nextGeneration();//prosegui di una generazione
-	void switchCellStatus(int x, int y);//cambia lo stato della cella alle coordinate x,y
-	void auto(int n);//procedi automaticamente di n generazioni
+	 int getGenTime();
+	 void switchCellState(int x, int y);
+	 void onStart();
+	 void onStop();
+	 void onClear();
+	 int numEpoch();
 }
