8c8a7b87f213a7c77afb9890eb2cdaca5dfcd0ec
FRoffia <francescoroffia@gmail.com>
2026-03-17 14:46:35 +0100

    fix sprint 3 distribuito

--- ConwayLife/Sprint3_distribuito/conway26GuiHtml/src/main/java/conwayGuiPageServer/MainConwayGui.java
+++ ConwayLife/Sprint3_distribuito/conway26GuiHtml/src/main/java/conwayGuiPageServer/MainConwayGui.java
@@ -1,13 +1,17 @@
-package conway26appl;
+package conwayGuiPageServer;
 import conway.io.IoJavalin;
 //import main.java.conway.devices.OutInWs;
 //import main.java.conway.domain.*;
 import unibo.basicomm23.utils.CommUtils;
 public class MainConwayGui  {
-   	private IoJavalin server = new IoJavalin();
+
+   	
+   	public MainConwayGui() {
+	    CommUtils.outgreen("MainConway | STARTS " );  
+  		new IoJavalin("guiserver");
+  	}
   	
     public static void main(String[] args) {
-	    System.out.println("MainConway | STARTS " );  
 	    
 		var resource = MainConwayGui.class.getResource("/page");
 		CommUtils.outgreen("DEBUG: La cartella /page si trova in: " + resource);
